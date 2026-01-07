9a9f821c78f76c214c4559de9b10fb913ed49df5
prakash-reddy-patil <patilprakashreddy@gmail.com>
2026-01-07 21:58:19 +0530

    terraform

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "sg_name" {
+    type = string
+}
+
+variable "sg_description" {
+    type = string
+    default = {}
+}
+
+variable "vpc_id" {
+    type = string
+}
+variable "sg_tags" {
+    type = string
+    default = {}
+}
